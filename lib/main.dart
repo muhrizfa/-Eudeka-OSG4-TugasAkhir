ed4cca0fc7c667a849ebf48a0e407e91c129b24d
muhrizfa <muhammadrizfa@gmail.com>
2019-07-08 19:25:58 +0700

    First Step

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Yu-Gi-Oh!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Yu-Gi-Oh! Card Deck'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +27,111 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body:
+            null // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
+
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:tugasakhir_eudeka/model.dart';
+// import 'package:http/http.dart' as http;
+
+// final String jsonplaceholder = "https://db.ygoprodeck.com/api/v5/cardinfo.php";
+
+// //Future method to read the URL
+// Future<Cardinfo> fetchInfo() async {
+//   final response = await http.get(jsonplaceholder);
+//   final jsonresponse = json.decode(response.body);
+
+//   return Cardinfo.fromJson(jsonresponse[0]);
+// }
+
+// void main() {
+//   runApp(new MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(
+//       home: new MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   final List<Cardinfo> cardinfo;
+//   final List<CardImage> cardImages;
+
+//   const MyHomePage({Key key, this.cardinfo, this.cardImages}) : super(key: key);
+
+//   @override
+//   _MyHomePageState createState() => new _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//         appBar: new AppBar(
+//           title: new Text("Parse JSON"),
+//         ),
+//         body: new Center(
+//           child: new Column(
+//             children: <Widget>[
+//               new FutureBuilder(
+//                   future: fetchInfo(),
+//                   builder: (context, snapshot) {
+//                     if (snapshot.hasData) {
+//                       return new Column(
+//                         children: <Widget>[
+//                           new Card(
+//                             shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(15.0),
+//                             ),
+//                             color: Colors.white,
+//                             child: ListTile(
+//                               leading: CircleAvatar(
+//                                 backgroundImage: NetworkImage(
+//                                     snapshot.data.cardImages[0].imageUrlSmall),
+//                               ),
+//                               title: new Container(
+//                                 margin: EdgeInsets.only(top: 10.0),
+//                                 child: Text(
+//                                   snapshot.data.name,
+//                                   style: TextStyle(
+//                                     fontSize: 15.0,
+//                                     fontWeight: FontWeight.bold,
+//                                   ),
+//                                 ),
+//                               ),
+//                               subtitle: new Container(
+//                                 margin:
+//                                     EdgeInsets.only(top: 10.0, bottom: 10.0),
+//                                 child: Text(snapshot.data.desc),
+//                               ),
+//                               trailing: Icon(Icons.keyboard_arrow_right),
+//                               onTap: () {},
+//                               selected: true,
+//                             ),
+//                           ),
+//                         ],
+//                       );
+//                     } else if (snapshot.hasError) {
+//                       return new Text("Error ${snapshot.error}");
+//                     }
+//                   })
+//             ],
+//           ),
+//         ));
+//   }
+// }
